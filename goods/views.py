e2b631c103bd275f2cba15deedb440d2aadf656c
Tetiana Malik <maliktetianamykolaivna@gmail.com>
2025-11-21 16:51:01 +0200

    Full text search done

--- goods/views.py
+++ goods/views.py
@@ -2,15 +2,19 @@ from django.shortcuts import get_list_or_404, render
 from django.core.paginator import Paginator
 
 from goods.models import Products
+from goods.utils import q_search
 
 
-def catalog(request, category_slug):
+def catalog(request, category_slug=None):
     page = request.GET.get('page', 1)
     on_sale = request.GET.get('on_sale', None)
     order_by = request.GET.get('order_by', None)
+    query = request.GET.get('q', None)
 
     if category_slug == 'all':
         goods = Products.objects.all()
+    elif query:
+        goods = q_search(query)
     else:
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
 
@@ -29,6 +33,7 @@ def catalog(request, category_slug):
         'slug_url': category_slug,                
     }
     return render(request, 'goods/catalog.html', context=context)
+
 
 def product(request, product_slug):
     product = Products.objects.get(slug=product_slug)
